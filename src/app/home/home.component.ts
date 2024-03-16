0dbde7f092371ef0223aa21dc695fe5a9191ea8e
stdbob <bobber1@yahoo.com>
2024-03-16 18:16:51 +0200

    Lesson 3: Create the application’s HousingLocation component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HousingLocationComponent } from '../housing-location/housing-location.component';
+
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, HousingLocationComponent],
   template: `
   <section>
     <form>
@@ -11,7 +14,10 @@ import { Component } from '@angular/core';
       <button class="primary" type="button">Search</button>
     </form>
   </section>
-`,
+  <section class="results">
+    <app-housing-location></app-housing-location>
+  </section>
+  `,
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
